7ee3ef47b47ca56ec454c333c92d7e4218d2a588
920164255@qq.com <920164255@qq.com>
2020-05-27 19:27:19 +0800

    update: 增加系统错误日志的打印

--- multi_cv2.py
+++ multi_cv2.py
@@ -9,6 +9,7 @@ from myutil import cleaningBoxes, getShortestDistance
 import configparser
 from Logger import *
 from detect_util import model
+import traceback
 
 evade_save_path = "D:/workspace/evade_save_path/"
 cf = configparser.ConfigParser()
@@ -27,6 +28,7 @@ n = len(ipArr)
 
 capList = []
 loggerList = []
+errorlog = Logger(os.path.join('./logs/error.log'), level='info')
 for i in range(len(ipArr)):
     dev = inputArr[i] if inputArr[i] != "0" else 0
     capList.append(cv2.VideoCapture(dev))
@@ -44,66 +46,69 @@ for i in range(len(capList)):
 count = 0
 while True:
     for i in range(len(capList)):
-        cap = capList[i]
-        ret, frame = cap.read()
-        if frame is None:
-            break
-        # print("frame: ", type(frame))
-        count += 1
-        start = time.time()
-        # print("count:", count)
-        results = model.detect([frame], verbose=1)
+        try:
+            cap = capList[i]
+            ret, frame = cap.read()
+            if frame is None:
+                break
+            # print("frame: ", type(frame))
+            count += 1
+            start = time.time()
+            # print("count:", count)
+            results = model.detect([frame], verbose=1)
 
-        # Visualize results
-        r = results[0]
-        # print(r['rois'], r['class_ids'])
+            # Visualize results
+            r = results[0]
+            # print(r['rois'], r['class_ids'])
 
-        # r['rois']，为矩形框的坐标：y1, x1, y2, x2 = boxes[i]，range of interest
-        # r['class_ids']，类别id
-        # r['scores']，类别得分
-        # save_instances(frame, , r['masks'], r['class_ids'],
-        #                class_names, os.path.join(output_path, name), r['scores'])
+            # r['rois']，为矩形框的坐标：y1, x1, y2, x2 = boxes[i]，range of interest
+            # r['class_ids']，类别id
+            # r['scores']，类别得分
+            # save_instances(frame, , r['masks'], r['class_ids'],
+            #                class_names, os.path.join(output_path, name), r['scores'])
 
-        content_list = [1, 15, 16]
-        personList = []
-        for box, class_id in zip(r['rois'], r['class_ids']):
-            if class_id in content_list:
+            content_list = [1, 15, 16]
+            personList = []
+            for box, class_id in zip(r['rois'], r['class_ids']):
+                if class_id in content_list:
+                    top, left, bottom, right = box
+                    personList.append([top, left, bottom, right])
+            #         print("personList--box: ", type(box), box)
+            # print("personList: ", personList)
+            afterCleanList = cleaningBoxes(personList)  # boxes清洗：area<阀值 && iou4small>0.45
+            # print("afterCleanList:", afterCleanList)
+            taopiaoList = getShortestDistance("", count, afterCleanList, None, None)  # 计算欧式距离，判断逃票
+            img = Image.fromarray(frame.astype('uint8'))  # numpy.ndarray 转PIL
+            thickness = 3
+            color = (0, 0, 255)  # BGR
+            for box in taopiaoList:
+                draw = ImageDraw.Draw(img)  # PIL中色彩是RGB
                 top, left, bottom, right = box
-                personList.append([top, left, bottom, right])
-        #         print("personList--box: ", type(box), box)
-        # print("personList: ", personList)
-        afterCleanList = cleaningBoxes(personList)  # boxes清洗：area<阀值 && iou4small>0.45
-        # print("afterCleanList:", afterCleanList)
-        taopiaoList = getShortestDistance("", count, afterCleanList, None, None)  # 计算欧式距离，判断逃票
-        img = Image.fromarray(frame.astype('uint8'))  # numpy.ndarray 转PIL
-        thickness = 3
-        color = (0, 0, 255)  # BGR
-        for box in taopiaoList:
-            draw = ImageDraw.Draw(img)  # PIL中色彩是RGB
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(img.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(img.size[0], np.floor(right + 0.5).astype('int32'))
-            print((left, top), (right, bottom))
+                top = max(0, np.floor(top + 0.5).astype('int32'))
+                left = max(0, np.floor(left + 0.5).astype('int32'))
+                bottom = min(img.size[1], np.floor(bottom + 0.5).astype('int32'))
+                right = min(img.size[0], np.floor(right + 0.5).astype('int32'))
+                print((left, top), (right, bottom))
 
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=color)
-                del draw
+                for i in range(thickness):
+                    draw.rectangle(
+                        [left + i, top + i, right - i, bottom - i],
+                        outline=color)
+                    del draw
 
-        result = np.asarray(img)
-        if isShow:
-            cv2.imshow("img", result)
-            cv2.waitKey(1)
-        if isOutput and len(taopiaoList) > 0:  # 只有存在逃票行为时才保存图片
-            save_path = os.path.join(evade_save_path, ipArr[i])
-            if os.path.exists(save_path) is False:
-                os.makedirs(save_path)
-            savefile = os.path.join(save_path, str(count) + "-" + str(int(time.time())) + ".jpg")
-            cv2.imwrite(savefile, result)
-            loggerList[i].logger.info("%s %s %s %s" % (str(count), savefile, taopiaoList, str(time.time() - start)))
-        else:
-            loggerList[i].logger.info("%s %s" % (str(count), str(time.time() - start)))
+            result = np.asarray(img)
+            if isShow:
+                cv2.imshow("img", result)
+                cv2.waitKey(1)
+            if isOutput and len(taopiaoList) > 0:  # 只有存在逃票行为时才保存图片
+                save_path = os.path.join(evade_save_path, ipArr[i])
+                if os.path.exists(save_path) is False:
+                    os.makedirs(save_path)
+                savefile = os.path.join(save_path, str(count) + "-" + str(int(time.time())) + ".jpg")
+                cv2.imwrite(savefile, result)
+                loggerList[i].logger.info("%s %s %s %s" % (str(count), savefile, taopiaoList, str(time.time() - start)))
+            else:
+                loggerList[i].logger.info("%s %s" % (str(count), str(time.time() - start)))
+        except Exception as e:
+            errorlog.logger.error(traceback.format_exc())
 
